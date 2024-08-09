5d84430d85d9fee47a0fb9638dd2ac0fb4381d22
sinclare210 <128484584+sinclare210@users.noreply.github.com>
2024-08-09 10:30:54 -0500

    okay
    not completed

---
+++ clipboard-landing-page-master/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        StrongCyan: "hsl(171, 66%, 44%)",
+        LightBlue: "hsl(233, 100%, 69%)",
+        DarkGrayishBlue: "hsl(210, 10%, 33%)",
+      GrayishBlue:"hsl(201, 11%, 66%)", 
+
+      },
+      fontFamily:{
+         bai: ['Bai Jamjuree', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+
